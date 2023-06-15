9a26080dc75aed49de2e5f5af00cc0022a7cafd8
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-06-15 16:38:46 -0500

    testing
    - testing case

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,15 @@
-def case_camel
-  puts 'CaseCamel'
+class MyList
+  def initialize(*list)
+    @list = list
+  end
+
+  def sum
+    @list.each do |element|
+      result = element * 10
+      puts result
+    end
+  end
 end
+
+list = MyList.new(10,10,10)
+list.sum
